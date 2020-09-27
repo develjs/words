5c2ebd53caf7c6b9c828588305388512acdd40b6
Alex <develjs@gmail.com>
2020-09-27 06:23:33 +0300

    refactoring

--- src/js/store.js
+++ src/js/store.js
@@ -1,5 +1,7 @@
 /*
 // todo:
+- add absoulte Level estamation for any text - index of words you should know to read this text
+- estimate user vocabulary
 - hightlight 1000 popular words
 - hightlight 2500? popular words
 - pack saved words by suffix
@@ -14,6 +16,7 @@
 */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {WordsHandler, wordsEx, wordsJoinedEx, removeSymbols, addWord, removeWord, parseText, doSort} from '../../lib/words';
 
 Vue.use(Vuex);
 
@@ -28,62 +31,33 @@ export const store = new Vuex.Store({
     getters: {
         // [{word:'word', count:1}]
         wordsEx: state => {
-            return state.words.map(word => {
-                return { word, count: state.counts[word]||1 }
-            })
+            return wordsEx(state.words, state.counts);
         },
 
         // [{word:'word', count:1}]
         wordsJoinedEx: state => {
-            let joined = [];
-            let sorted = [...state.words].sort();
-            sorted.forEach(word => {
-                let base = suffixless(word);
-                if (base != word) {
-                    let last = joined[joined.length-1];
-                    if (base && last.startsWith(base)) {
-                        if (suffixing(word).concat(base).indexOf(last)>=0) {
-                            joined[joined.length-1] += ',' + word;
-                            return;
-                        };
-                    }
-                }
-
-                joined.push(word);
-            });
-
-            return joined.map(word => {
-                let count = 0;
-                word.split(',').forEach(word => count += state.counts[word]||1);
-                return { word, count: count||1 }
-            })
+            return wordsJoinedEx(state.words, state.counts);
         }
     },
 
     mutations: {
+        // add word to list except knowns
         addWord: function(state, word) {
             if (!word) return;
             if (state.knowns.indexOf(word)>=0) return;
-                
-            if (state.words.indexOf(word)>=0) {
-                state.counts[word] = (state.counts[word]||1) + 1;
-                return;
-            }
-            state.words.push(word);
+            
+            addWord(word, state.words, state.counts);
         },
 
         parseText: function(state, text) {
-            text.split(/[\n\r\s]+/).forEach(value => {
-                value = removeSymbols(value, ' ').trim().toLowerCase();
+            parseText(text, value => {
                 this.commit('addWord', value);
-            });
+            })
             this.commit('doSort');
         },
 
         doSort: function(state) {
-            var words = state.words.slice();
-            words.sort((a,b) => (state.counts[b]||1) - (state.counts[a]||1) );
-            state.words = words;
+            doSort(state.words, state.counts);
         },
 
         addKnown(state, word) {
@@ -92,7 +66,7 @@ export const store = new Vuex.Store({
                 if (w<0) state.knowns.push(word);
                 this.commit('localSave');
     
-                state.words.splice(state.words.indexOf(word), 1);
+                removeWord(word, state.words);
                 console.log('hide', word);
             })
         },
@@ -145,36 +119,3 @@ export const store = new Vuex.Store({
 
     }
 });
-
-
-function removeSymbols (text, joinS) {
-    var symbs = '0123456789!"#$%&\()*+,./:;<=>?[\]^_{|}~“”…';
-    var symb2 = ['&nbsp;',/’s$/,/’ll$/,/’re$/,/n’t$/,/’ve#$/,/’ll$/,/[\u2000-\u20FF]/];
-  
-    for (let i=0; i < symb2.length; i++) {
-        text = text.split(symb2[i]).join(joinS || '');
-    }
-    /* var new_text = '';
-    for (var i=0; i<text.length; i++)
-        if (trueSymb.indexOf(text[i])>=0) new_text += text[i]; */
-    
-    for (let i=0; i<symbs.length; i++) {
-        text = text.split(symbs[i]).join(joinS || '');
-    }
-    return text;
-}
-
-const SUFFIXES = ['es', 's', 'ed', 'd', 'ly', 'ing'];//'ment', 'able', 'ible',
-function suffixing(word) {
-    return SUFFIXES.map(suff => {
-        word + suff
-    })
-}
-
-function suffixless(word) {
-    for (let s=0; s<SUFFIXES.length; s++) {
-        if (!word.endsWith(SUFFIXES[s])) continue;
-        return word.substr(0, word.length - SUFFIXES[s].length);
-    }
-    return word;
-}
